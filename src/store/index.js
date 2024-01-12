b428e4ea218d97425b125845897d500556e4ae18
CodeBroke33 <minedom33@gmail.com>
2024-01-12 16:58:14 +0200

    thats fine

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,43 @@
 import { createStore } from 'vuex'
-//  const dataUrl = 'https://github.com/CodeBroke33/EOMPPortfolioData.git'
+ const dataUrl = 'https://codebroke33.github.io/vuejsEOMPdata/data/'
  export default createStore({
   state: {
-    // jobTitle: null,
-    // about: null,
-    // education: null,
-    // skills: null,
-    // testimonials: null,
-    // projects:null
+    JobTitle: null,
+    about: null,
+    education: null,
+    skills: null,
+    testimonials: null,
+    projects:null
   },
   getters: {
   },
   mutations: {
+    
+    setJobtitle (state,value){
+      state.jobtitle = value
+    },
+    setAbout (state,value){
+      state.about = value
+    },
+    setEducation (state,value){
+      state.education = value
+    },
+    setSkills (state,value){
+      state.skills = value
+    },
+    setTestimonials (state,value){
+      state.testimonials = value
+    },
+    setProjects (state,value){
+      state.projects = value
+    },
   },
   actions: {
+    async fetchTestimonial(context){
+      let res = await fetch(dataUrl)
+      let {testimonials} =  res
+      context.commit('setTestimonials',testimonials)
+    }
   },
   modules: {
   }
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,7 +10,11 @@
     <span class="line"></span>
   </div>
   <div class="info">
-    <h1> </h1>
+    <h1>My Story</h1>
+    <div></div>
+    <div></div>
+    <div></div>
+   <p> Once upon a time, in the bustling world of high school, I was the merchant with pockets full of cash and dreams set on computers. Life was good until the day reality hit hard.It all began with a seemingly innocent suggestion from the principal: skip school on certain days for exams. Little did I know, those missed days would shatter my dream. When the results came in, those two missing subjects left my matric certificate incomplete, a haunting reminder of missed opportunities.Determined to pursue my passion for computers, I applied to the Life Choices Coding Academy. But my incomplete certificate was a barrier.</p> <p>Honesty led me to disclose the truth, leaving me disappointed and facing the daunting task of completing my certificate, a year-long process.Desperate to make ends meet, unemployed and disheartened, my grandmothers persistent nagging pushed me to seek employment. Months passed with no luck until a mysterious message led me to an agency that found me a job at Woolworths. It wasnt my dream, but it kept me afloat.As an OPS assistant, I observed those working with computers in the office, sparking a desire to move up. I seized the chance for promotion, climbing slowly, learning, and adapting. Attempts to study ICT were thwarted by COVID, and my savings vanished, yet I persisted, continuously learning new skills during my night shifts.Persistence paid off when my extra efforts caught managements attention. The question that changed everything: Would you be interested in working in the office? I leaped at the chance, becoming the youngest and least experienced to hold that position in Woolworths history.But the glamour faded. The office felt like a scapegoat hub, a place to dump blame. Despite success, dissatisfaction brewed. I loved my job but detested the environment and the paycheck.</p> <p>Thats when I decided to reapply to the Life Choices Coding Academy, seeking a path back to my true passion.Now, despite the discontent, I'm still at Woolworths, surrounded by unbearable colleagues and insufficient pay. But being accepted into the coding academy again promises a new chapter, a chance to fulfill my dreams. The certainty of a clearer future keeps me going, knowing that soon, Ill be immersed in what Ive always yearned for, a life with computers.Its been a journey of setbacks, resilience, and unyielding determination. And now, as I step into this new chapter, the hope of finally merging passion and profession lights the way for.</p>
   </div>
   </main>
 </template>
@@ -85,7 +89,11 @@ font-family:  Righteous;
 
 .info {
 font-family: Righteous !important;
-padding: 50% 60%;
+position: absolute;
+top: 55em;
+right: 2em;
+margin: 2em;
+color: white;
 
 }
 
